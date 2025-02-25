191a6607f12e95de0105b6b8964b285504bfbd07
bazimR <rishaalmuhammed@gmail.com>
2025-02-25 13:00:33 +0530

    added new Atronaut type and bundle extention function

--- Moon Shot/Bundle-Decodable.swift
+++ Moon Shot/Bundle-Decodable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) data.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String:Astronaut].self, from: data) else {
+            fatalError("Failed to decode from \(file)")
+        }
+        
+        return loaded
+    }
+}
