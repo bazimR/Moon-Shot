61263f9ea77aed1712bf1e4a3df6a26d453df9cf
bazimR <rishaalmuhammed@gmail.com>
2025-02-26 10:19:32 +0530

    added custom color theme

--- Moon Shot/ContentView.swift
+++ Moon Shot/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let column = [
+        GridItem(.adaptive(minimum: 120))
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: column) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("details page")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100).padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate).font(
+                                        .caption
+                                    ).foregroundColor(.white.opacity(0.6))
+                                }.frame(maxWidth: .infinity).padding()
+                                    .background(
+                                        .darkBackground
+                                    )
+                            }.clipShape(.rect(cornerRadius: 10)).overlay {
+                                RoundedRectangle(cornerRadius: 10).stroke(
+                                    .lightBackground)
+                            }
+                        }
+
+                    }
+                }.padding([.horizontal, .bottom])
+            }.navigationTitle("Moon Shot").preferredColorScheme(.dark)
         }
-        .padding()
     }
 }
 
